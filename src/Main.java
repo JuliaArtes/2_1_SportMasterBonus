695aff0208233e3d0ea9bbfb1e79ccdfdaa810e6
MaratArkhipov <maratkzn2010@yandex.ru>
2019-09-27 02:36:13 +0400

    fixes#4

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,14 @@ public class Main {
     public static void main(String[] args) {
         int costOfPreviousPenny = 150001_00;
         int costOfThisPenny = 2500_00;
-        int Bonus;
+        int bonus;
         if (costOfPreviousPenny > 150000_00) {
-            Bonus = 100 * (costOfThisPenny / 1000_00);
+            bonus = 100 * (costOfThisPenny / 1000_00);
         } else if (costOfPreviousPenny > 15000_00) {
-            Bonus = 70 * (costOfThisPenny / 1000_00);
+            bonus = 70 * (costOfThisPenny / 1000_00);
         } else if (costOfPreviousPenny > 0) {
-            Bonus = 50 * (costOfThisPenny / 1000_00);
-        } else Bonus = 0;
-        System.out.println(Bonus);
+            bonus = 50 * (costOfThisPenny / 1000_00);
+        } else bonus = 0;
+        System.out.println(bonus);
     }
 }
