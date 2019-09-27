c3390ef1acc1e2616c9992bd54338e9d854dff66
MaratArkhipov <maratkzn2010@yandex.ru>
2019-09-27 20:00:35 +0400

    fixes#3

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,20 @@
 public class Main {
     public static void main(String[] args) {
+        int standartLevel = 15000_00;
+        int silverLevel = 150000_00;
+        int standartBonus = 50;
+        int silverBonus = 70;
+        int goldBonus= 100;
+        int bonusAmountPenny = 1000_00;
         int costOfPreviousPenny = 150001_00;
         int costOfThisPenny = 2500_00;
         int bonus;
-        if (costOfPreviousPenny > 150000_00) {
-            bonus = 100 * (costOfThisPenny / 1000_00);
-        } else if (costOfPreviousPenny > 15000_00) {
-            bonus = 70 * (costOfThisPenny / 1000_00);
+        if (costOfPreviousPenny > silverLevel) {
+            bonus = goldBonus * (costOfThisPenny / bonusAmountPenny);
+        } else if (costOfPreviousPenny > standartLevel) {
+            bonus = silverBonus * (costOfThisPenny / bonusAmountPenny);
         } else if (costOfPreviousPenny > 0) {
-            bonus = 50 * (costOfThisPenny / 1000_00);
+            bonus = standartBonus * (costOfThisPenny / bonusAmountPenny);
         } else bonus = 0;
         System.out.println(bonus);
     }
